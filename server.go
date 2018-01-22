2944e0c344d5fce24591d2b84b227d39e74ba4ee
Bryan Boreham <bjboreham@gmail.com>
2018-01-22 16:45:04 +0000

    Increase latency

--- server.go
+++ server.go
@@ -35,7 +35,7 @@ func logoHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "logo.png")
 }
 
-var sleepTime = time.Duration(10 * time.Millisecond)
+var sleepTime = time.Duration(2000 * time.Millisecond)
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
